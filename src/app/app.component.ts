042b92e569b69f3c7c44307a91f07916100c8069
Miro-Jankech <jankechmiro@gmail.com>
2025-08-25 10:30:10 +0200

    RxJS completed

---
+++ src/app/app.component.ts
@@ -0,0 +1,78 @@
+import { Component, DestroyRef, OnInit, effect, inject, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+
+import { interval, map, filter, subscribeOn, Subscription, Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount)
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$, {initialValue:10});
+
+
+
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      // subscriber.error()
+      if (timesExecuted > 3) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value...');
+       timesExecuted++;
+    },2000);
+  })
+
+
+
+
+  constructor() {
+   // effect(() => {
+     // console.log(`Clicked button ${this.clickCount()} times`);  
+    }
+  //);
+  
+  
+
+  private destroyRef = inject(DestroyRef)
+
+  ngOnInit() {
+     /* const subscription = interval(1000).pipe(
+        map((val) => val*2),
+        filter((val) => val > 10)
+      ).subscribe({
+        next: (val) => console.log(val)   
+      }); 
+    
+      this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe();
+  }); */
+
+    this.customInterval$.subscribe({
+      next:(val) => console.log(val),
+      complete: () => console.log("COMPLETED!"),
+      error: (err) => console.log(err)
+      
+    });
+
+    const subscription = this.clickCount$.subscribe({
+      next:(val) => console.log(`Clicked button ${this.clickCount()} times`) 
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+
+  onClick() {
+    this.clickCount.update((prevCount => prevCount+1))
+  }
+ 
+}
